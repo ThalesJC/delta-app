e3e03cbe1325868a85c2392e2356800638af92c4
Thales Colovini <thales.soul96@gmail.com>
2025-01-07 18:28:02 -0300

    feat: add header component & logo

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/header/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar
+      backgroundColor='#0083C0'
+      style='light'
+      />
+      <Header />
     </View>
   );
 }
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,21 @@
+import { Image, View, StyleSheet, Text } from "react-native";
+
+export default function Header() {
+  return (
+    <View style={styles.header}>
+      <Image source={require('../../../assets/smallLogo.png')} />
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  header: {
+    height: '15%',
+    width: '100%',
+    backgroundColor: '#0083C0',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    paddingLeft: 16,
+  },
+});
